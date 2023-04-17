ffe1e0b7b2c6a76e510ca1975f30d31fd8b7766a
OkadaJ <okadajorge@gmx.de>
2023-04-17 17:22:54 +0200

    .

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': process.env.API_KEY,
+            'X-RapidAPI-Key': '00a9dfc0ebmsh626f140b6dd63f1p1ee20ajsn4a1a771bf9b5',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     })
